d79c48dcd3b50156ecedf6c5048fab9509435c90
sberbom <sberbom@gmail.com>
2021-06-18 15:51:57 +0200

    laget app-skjelett

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Knapp } from 'nav-frontend-knapper';
+import { Checkbox } from 'nav-frontend-skjema';
+import "./app.less"
+
+const App = () => {
+  return (
+    <>
+      <h1>My React and TypeScript App! {" "} {new Date().toLocaleDateString()}</h1>
+      <Knapp type={"standard"}>HEI</Knapp>
+      <Checkbox label={"Checkbox"} />
+    </>
+  )
+};
+
+export default App;
---
+++ src/Foo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Foo = () => {
+    return <h1>Teamfamilie! {" "} {new Date().toLocaleDateString()}</h1>
+};
+
+export default Foo;
