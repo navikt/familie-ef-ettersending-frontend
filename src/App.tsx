f56c690803ca05956081b4b3caa4a389286b29d0
Simen <simentengs@gmail.com>
2021-06-21 00:24:02 +0200

    tester pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ const App = () => {
   return (
     <>
       <p>Dette er en endring</p>
-
-
-      
       <h1>My React and TypeScript App! {" "} {new Date().toLocaleDateString()}</h1>
       <Knapp type={"standard"}>HEI</Knapp>
       <Checkbox label={"Checkbox"} />
