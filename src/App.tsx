c234f71943072ca0859f9636e791dd33a4221a1e
Simen <simentengs@gmail.com>
2021-06-20 17:15:08 +0200

    test error

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./app.less"
 const App = () => {
   return (
     <>
+      <p>Dette er en endring</p>
       <h1>My React and TypeScript App! {" "} {new Date().toLocaleDateString()}</h1>
       <Knapp type={"standard"}>HEI</Knapp>
       <Checkbox label={"Checkbox"} />
