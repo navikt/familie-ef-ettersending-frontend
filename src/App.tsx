d621c8d606d96272d9eb9bff7b87aa5f6a14a315
Simen <simentengs@gmail.com>
2021-06-21 00:34:32 +0200

    tester pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React from 'react';
 import { Knapp } from 'nav-frontend-knapper';
 import { Checkbox } from 'nav-frontend-skjema';
-import "./app.less"
+import './app.less';
 
 const App = () => {
   return (
     <>
       <p>Dette er en endring</p>
-      <h1>My React and TypeScript App! {" "} {new Date().toLocaleDateString()}</h1>
-      <Knapp type={"standard"}>HEI</Knapp>
-      <Checkbox label={"Checkbox"} />
+
+      <h1>My React and TypeScript App! {new Date().toLocaleDateString()}</h1>
+      <Knapp type={'standard'}>HEI</Knapp>
+      <Checkbox label={'Checkbox'} />
     </>
-  )
+  );
 };
 
 export default App;
