df3d731e95f18931f78085925844d3d54177baa2
Simen <simentengs@gmail.com>
2021-06-24 12:06:37 +0200

    lagt til skall-struktur for react.context

--- src/frontend/context/AppContext.ts
+++ src/frontend/context/AppContext.ts
@@ -3,8 +3,6 @@ import React, { createContext } from 'react';
 
 import createUseContext from 'constate';
 
-//createUseContext?
-
 const [AppProvider, useApp] = createUseContext(() => {
   const [testVerdi, setTestVerdi] = useState('Dette er fra Appcontext');
 
