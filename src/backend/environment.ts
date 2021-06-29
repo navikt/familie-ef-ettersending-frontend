92d577c640bbc5cc9091e356723d118857f77cea
sberbom <sberbom@gmail.com>
2021-06-29 14:04:28 +0200

    logger env variabler

--- src/backend/environment.ts
+++ src/backend/environment.ts
@@ -1,5 +1,6 @@
 const getEnv = () => {
   if (process.env.ENV === 'prod') {
+    console.log('Miljø -> Prod');
     return {
       loginService: 'https://loginservice.nav.no/login?',
       apiUrl: 'https://www.nav.no/familie/alene-med-barn/soknad-api',
@@ -7,6 +8,7 @@ const getEnv = () => {
       port: 9000,
     };
   } else if (process.env.ENV === 'dev') {
+    console.log('Miljø -> Dev');
     return {
       loginService: 'https://loginservice.dev.nav.no/login?',
       apiUrl: 'https://www-q0.nav.no/familie/alene-med-barn/soknad-api',
@@ -14,6 +16,7 @@ const getEnv = () => {
       port: 9000,
     };
   } else {
+    console.log('Miljø -> Prod or dev not defined');
     return {
       loginService: 'https://loginservice.dev.nav.no/login?',
       apiUrl: 'http://localhost:8091',
