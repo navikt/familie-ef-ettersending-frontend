c91667e7d4068b9f8c095667e113d0d8f45ac5a8
ingebjorg <ingebjrgb@gmail.com>
2021-06-29 08:56:31 +0200

    hent data fra api

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.less';
 import Filopplasting from './komponenter/Filopplasting';
-
 import DisplayContext from './komponenter/DisplayContext';
 import InputForm from './komponenter/InputForm';
+import { Knapp } from 'nav-frontend-knapper';
+import axios from 'axios';
+import { response } from 'express';
 
 const App = () => {
-  return (
+  const [navn, settNavn] = useState('');
 
+  const hentData = () => {
+    return axios
+      .get('http://localhost:8091/api/oppslag/sokerinfo', {
+        withCredentials: true,
+      })
+      .then((response: { data: any }) => {
+        console.log(response.data);
+      });
+  };
+
+  const hentPerson = () => {
+    return axios
+      .get('http://localhost:8091/api/oppslag/sokerinfo', {
+        withCredentials: true,
+      })
+      .then((response: { data: any }) => {
+        console.log(response.data.søker.forkortetNavn);
+        settNavn(response.data.søker.forkortetNavn);
+      });
+  };
+
+  hentData();
+  hentPerson();
+
+  return (
     <div className="bakgrunn">
       <div className="app-konteiner">
-        <h1>Ettersending av dokumentasjon</h1>
+        <h1>Ettersending av dokumentasjon for {navn}</h1>
         <Filopplasting />
         <Knapp className="innsendingsknapp" type={'standard'}>
           Send inn
