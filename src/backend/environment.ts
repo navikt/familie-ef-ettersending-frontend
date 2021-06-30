f0ca2c8bfb184edfcc520683a0fc0d1b8902dc78
sberbom <sberbom@gmail.com>
2021-06-30 14:04:26 +0200

    fjernet debug logging

--- src/backend/environment.ts
+++ src/backend/environment.ts
@@ -14,7 +14,6 @@ const isDev = () => {
 
 const getEnv = () => {
   if (isProd()) {
-    console.log('Miljø -> Prod');
     return {
       loginService: 'https://loginservice.nav.no/login?',
       apiUrl: 'https://www.nav.no/familie/alene-med-barn/soknad-api',
@@ -22,7 +21,6 @@ const getEnv = () => {
       port: 9000,
     };
   } else if (isDev()) {
-    console.log('Miljø -> Dev');
     return {
       loginService: 'https://loginservice.dev.nav.no/login?',
       apiUrl:
@@ -31,7 +29,6 @@ const getEnv = () => {
       port: 9000,
     };
   } else {
-    console.log('Miljø -> Prod or dev not defined');
     return {
       loginService: 'https://loginservice.dev.nav.no/login?',
       apiUrl: 'http://localhost:8091',
