8be435cb14f607f8bf0469d897b473d914a9956e
ingebjorg <ingebjrgb@gmail.com>
2021-07-06 15:15:11 +0200

    legger til korrekt url i sendVedleggs post request

--- src/frontend/api-service.ts
+++ src/frontend/api-service.ts
@@ -38,10 +38,16 @@ export const hentDokumentasjonsbehov = () => {
 
 export const sendVedlegg = (formData): Promise<string> => {
   return axios
-    .post(`${environment().dokumentUrl}`, formData, {
-      headers: { 'content-type': 'multipart/form-data' },
-      withCredentials: true,
-    })
+    .post(
+      `${
+        environment().dokumentUrl
+      }/familie/dokument/api/mapper/familievedlegg/`,
+      formData,
+      {
+        headers: { 'content-type': 'multipart/form-data' },
+        withCredentials: true,
+      }
+    )
     .then((response: { data: any }) => {
       return response.data;
     })
