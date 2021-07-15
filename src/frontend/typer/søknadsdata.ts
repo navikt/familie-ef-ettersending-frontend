0b316b951ad81bd65ec997e0fe3307edd9cae53b
ingebjorg <ingebjrgb@gmail.com>
2021-07-15 13:18:14 +0200

    lager ny type ISøknadsbehov

--- src/frontend/typer/søknadsdata.ts
+++ src/frontend/typer/søknadsdata.ts
@@ -1,3 +1,12 @@
+import { IDokumentasjonsbehov } from './dokumentasjonsbehov';
+
+export interface ISøknadsbehov {
+  dokumentasjonsbehov: Array<IDokumentasjonsbehov>;
+  innsendingstidspunkt: any;
+  personIdent: string;
+  søknadType: string;
+}
+
 export interface IVedlegg {
   id: string;
   navn: string;
