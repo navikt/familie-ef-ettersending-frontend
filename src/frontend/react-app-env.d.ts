08768f75bca63d1f2d45d0ddd43a8d271c07f78a
sberbom <sberbom@gmail.com>
2021-07-21 10:51:31 +0200

    lagt til ts for frontend

---
+++ src/frontend/react-app-env.d.ts
@@ -0,0 +1,16 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly PUBLIC_URL: string;
+  }
+}
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
