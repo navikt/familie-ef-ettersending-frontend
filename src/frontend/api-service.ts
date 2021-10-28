253e6902ce89e5afdbc3c8fad2c3d2007dfeb865
Johan Blomgren <blommish@users.noreply.github.com>
2021-10-28 13:39:40 +0200

    Sender med headers til backend for å kunne finne frem hvilken app som kallet på soknad-api/dokument (#160)

--- src/frontend/api-service.ts
+++ src/frontend/api-service.ts
@@ -13,19 +13,30 @@ interface IKvittering {
   mottattDato: string;
 }
 
+const HEADER_NAV_CONSUMER_ID = 'Nav-Consumer-Id';
+const HEADER_NAV_CONSUMER_ID_VALUE = 'familie-ef-ettersending';
+
 export const sendEttersending = (
   ettersendingsdata: IEttersending
 ): Promise<IKvittering> => {
   return axios
     .post(`${environment().apiUrl}/api/ettersending`, ettersendingsdata, {
       withCredentials: true,
+      headers: {
+        [HEADER_NAV_CONSUMER_ID]: HEADER_NAV_CONSUMER_ID_VALUE,
+      },
     })
     .then((response) => response.data);
 };
 
 export const hentEttersendinger = (): Promise<IEttersending[]> => {
   return axios
-    .get(`${environment().apiUrl}/api/ettersending`, { withCredentials: true })
+    .get(`${environment().apiUrl}/api/ettersending`, {
+      withCredentials: true,
+      headers: {
+        [HEADER_NAV_CONSUMER_ID]: HEADER_NAV_CONSUMER_ID_VALUE,
+      },
+    })
     .then((response: { data: IEttersending[] }) => response.data);
 };
 
@@ -33,6 +44,9 @@ export const hentPersoninfo = (): Promise<IPersoninfo> => {
   return axios
     .get(`${environment().apiUrl}/api/oppslag/sokerinfo`, {
       withCredentials: true,
+      headers: {
+        [HEADER_NAV_CONSUMER_ID]: HEADER_NAV_CONSUMER_ID_VALUE,
+      },
     })
     .then((response: { data: IPersoninfo }) => response.data);
 };
@@ -41,6 +55,9 @@ export const hentSøknader = (): Promise<ISøknadsbehov[]> => {
   return axios
     .get(`${environment().apiUrl}/api/dokumentasjonsbehov/person`, {
       withCredentials: true,
+      headers: {
+        [HEADER_NAV_CONSUMER_ID]: HEADER_NAV_CONSUMER_ID_VALUE,
+      },
     })
     .then((response: { data: ISøknadsbehov[] }) => response.data);
 };
@@ -50,7 +67,10 @@ export const sendVedleggTilMellomlager = (
 ): Promise<string> => {
   return axios
     .post(`${environment().dokumentUrl}`, formData, {
-      headers: { 'content-type': 'multipart/form-data' },
+      headers: {
+        'content-type': 'multipart/form-data',
+        [HEADER_NAV_CONSUMER_ID]: HEADER_NAV_CONSUMER_ID_VALUE,
+      },
       withCredentials: true,
     })
     .then((response: { data: Ifamilievedlegg }) => response.data.dokumentId);
