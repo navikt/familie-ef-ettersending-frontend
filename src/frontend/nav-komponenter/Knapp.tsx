fd1eb61ea350c9a5e8a778e1179b940bd0e408a4
Viktor <viktor.grondalen.solberg@nav.no>
2022-02-04 10:24:47 +0100

    Fix padding til knapper

--- src/frontend/nav-komponenter/Knapp.tsx
+++ src/frontend/nav-komponenter/Knapp.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Button } from '@navikt/ds-react';
 
 const KnappMedPadding = styled(Button)`
-  padding-left: var(--navds-spacing-10);
-  padding-right: var(--navds-spacing-10);
+  padding-left: 3rem;
+  padding-right: 3rem;
 `;
 export default KnappMedPadding;
