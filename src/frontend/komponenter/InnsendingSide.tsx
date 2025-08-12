3ba7fcaabeaeb8a4443bfdc21e6389513335532c
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2025-08-12 10:39:23 +0200

    Fjerner unødvendig setup med postcss og autoprefixer. Fortsatt med fjerning a styled components (#951)

--- src/frontend/komponenter/InnsendingSide.tsx
+++ src/frontend/komponenter/InnsendingSide.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { DokumentasjonsbehovListe } from './DokumentasjonsbehovListe';
 import { EkstraDokumentasjonsbehovBoks } from './EkstraDokumentasjonsbehovBoks';
 import { IDokumentasjonsbehov, IEttersending } from '../typer/ettersending';
-import styled from 'styled-components';
-import KnappMedPadding from '../felles/Knapp';
 import { alertMelding } from './AlertStripe';
 import { AddCircle } from '@navikt/ds-icons';
-import { Button } from '@navikt/ds-react';
+import { Button, VStack } from '@navikt/ds-react';
 
 interface IProps {
   ettersending: IEttersending;
@@ -18,16 +16,6 @@ interface IProps {
   ekstraInnsendingerUtenVedlegg: string[];
   settAlertStripeMelding: (melding: alertMelding) => void;
 }
-
-const StyledDiv = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const HovedKnapp = styled(KnappMedPadding)`
-  margin: 1rem;
-  margin-bottom: 0;
-`;
 
 export const InnsendingSide: React.FC<IProps> = ({
   ettersending,
@@ -75,9 +63,9 @@ export const InnsendingSide: React.FC<IProps> = ({
       >
         Legg til flere dokumenter
       </Button>
-      <StyledDiv>
-        <HovedKnapp onClick={visOppsummering}>Neste</HovedKnapp>
-      </StyledDiv>
+      <VStack align={'center'}>
+        <Button onClick={visOppsummering}>Neste</Button>
+      </VStack>
     </>
   );
 };
