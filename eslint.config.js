37d566c0d34d79b12f206cd9393312f37eaa256a
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-01-19 12:39:59 +0100

    Importerer react i appContext. Legger til eslint regel om at filer jsx filer må importere react.

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,31 @@
 import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        languageOptions: {
-            parserOptions: { ecmaFeatures: { jsx: true } },
-        },
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    {
-        plugins: { 'react-hooks': reactHooks },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react/no-unescaped-entities': 'off',
-            '@typescript-eslint/no-unused-expressions': 'off'
-        },
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
+  },
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      react: react,
     },
-);
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'error',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
